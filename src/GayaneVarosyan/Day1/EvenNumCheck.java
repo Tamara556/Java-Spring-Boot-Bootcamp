7d7bd27d133ae630adbc542563121839a2025563
TamaraMurazyan <murazyan.tamara5566@gmail.com>
2026-02-23 23:34:57 +0400

    Day 1 fixes

---
+++ src/GayaneVarosyan/Day1/EvenNumCheck.java
@@ -0,0 +1,11 @@
+package GayaneVarosyan.Day1;
+
+public class EvenNumCheck {
+    public static boolean evenNumCheck(int num) {
+        return num % 2 == 0;
+    }
+    static void main() {
+        int num = 10;
+        System.out.println(evenNumCheck(num));
+    }
+}
